a0d14769117950bb2b74a790a89790c889d2643a
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2022-03-01 16:33:50 -0800

    update page

--- mycrypto/__init__.py
+++ mycrypto/__init__.py
@@ -7,24 +7,29 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+
+##Add your db seetings here
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://rootuser-here:password-here@localhost/dbname-here"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'rayiszafar@gmail.com'
-app.config['MAIL_PASSWORD'] = 'wzknjhbulyuzrjop'
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+
+##Please change
+app.config["MAIL_USERNAME"] = "rayiszafar@gmail.com"
+##Please change
+app.config["MAIL_PASSWORD"] = "wzknjhbulyuzrjop"
+
+
 mail = Mail(app)
-
-
-
-
 from mycrypto import routes
